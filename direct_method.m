1810e5f5bd3fe930d6efa78db040a4dbf4c18fac
satyaj99t <ee170002042@iiti.ac.in>
2020-05-27 11:42:45 +0530

    Copy old input method. Solve the equation for real value of roots.

---
+++ direct_method.m
@@ -0,0 +1,33 @@
+% Initialize
+clear ; close all; clc
+
+% Take in k1, k2, k3, k4
+kvector = input('Enter value of [k1 k2 k3 k4] = ');
+
+% The root polynomial looks like as
+% a*s^4 + b*s^3 c*s^2 + d*s^1 + e + K = 0
+
+% Now we will commpute values of these coeff
+
+a=1;
+b=0;
+c=0;
+d=0;
+e=1;
+
+for i = 1:4
+    b = b + kvector(i);
+    e = e*kvector(i);
+    for j = i+1:4
+        c = c + kvector(i)*kvector(j);
+        for k = j+1:4
+            d = d + kvector(i)*kvector(j)*kvector(k);
+        end
+    end
+end
+
+K = sym('K');
+s = sym('s');
+eqn = a*s^4 + b*s^3 + c*s^2 + d*s^1 + e + K;
+
+S = solve(eqn,s,'Real',true);
